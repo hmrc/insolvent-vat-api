4934cb6ac306b4e4da972b897fbfea5857764424
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2021-04-27 14:48:31 +0100

    MTDSA 8700 Code cleanup

--- app/utils/IdGenerator.scala
+++ app/utils/IdGenerator.scala
@@ -18,10 +18,10 @@ package utils
 
 import java.util.UUID
 
-import javax.inject.{Inject, Singleton}
+import javax.inject.Singleton
 
 @Singleton
-class IdGenerator @Inject()() {
+class IdGenerator {
 
   def generateCorrelationId: String = UUID.randomUUID().toString
 }
