d5dc3465d59db5ab3365780bdf0e30d75fb288d8
“ngangaNjiraini” <52919747+ngangaNjiraini@users.noreply.github.com>
2021-04-28 15:10:12 +0100

    MTDSA-8702 Added Framework for SumbitReturn RequestParser and Validator

---
+++ app/v1/controllers/requestParsers/validators/SubmitReturnValidator.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import v1.controllers.requestParsers.validators.validations.{DateFormatValidation, DecimalValueValidation, JsonFormatValidation, NonDecimalValueValidation, PeriodKeyValidation, UniqueIDValidation, ValueFormatErrorMessages, VrnValidation}
+import v1.models.errors.MtdError
+import v1.models.request.{SubmitReturnRawData, SubmitReturnRequestBody}
+
+class SubmitReturnValidator extends Validator[SubmitReturnRawData] with ValueFormatErrorMessages{
+
+  private val validationSet = List(parameterFormatValidation, bodyValueValidator)
+
+  private def parameterFormatValidation: SubmitReturnRawData => List[List[MtdError]] = (data: SubmitReturnRawData) => {
+    List(
+      VrnValidation.validate(data.vrn),
+      JsonFormatValidation.validate[SubmitReturnRequestBody](data.body.json)
+    )
+  }
+
+  private def bodyValueValidator: SubmitReturnRawData => List[List[MtdError]] = { data =>
+
+    val requestBodyData = data.body.json.as[SubmitReturnRequestBody]
+
+    List(
+      PeriodKeyValidation.validate(requestBodyData.periodKey),
+      DecimalValueValidation.validate(
+        amount = requestBodyData.vatDueSales,
+        path = "/vatDueSales",
+        minValue = -9999999999999.99,
+      ),
+      DecimalValueValidation.validate(
+        amount = requestBodyData.vatDueAcquisitions,
+        path = "/vatDueAcquisitions",
+        minValue = -9999999999999.99,
+      ),
+      DecimalValueValidation.validate(
+        amount = requestBodyData.totalVatDue,
+        path = "/totalVatDue",
+        minValue = -9999999999999.99,
+      ),
+      DecimalValueValidation.validate(
+        amount = requestBodyData.vatReclaimedCurrPeriod,
+        path = "/vatReclaimedCurrPeriod",
+        minValue = -9999999999999.99,
+      ),
+      DecimalValueValidation.validate(
+        amount = requestBodyData.netVatDue,
+        path = "/netVatDue",
+        maxValue = 99999999999.99,
+        message = ZERO_MINIMUM_INCLUSIVE
+      ),
+      NonDecimalValueValidation.validate(
+        amount = requestBodyData.totalValueSalesExVAT,
+        path = "/totalValueSalesExVAT",
+        minValue = -9999999999999.00,
+        maxValue = 9999999999999.00
+      ),
+      NonDecimalValueValidation.validate(
+        amount = requestBodyData.totalValuePurchasesExVAT,
+        path = "/totalValuePurchasesExVAT",
+        minValue = -9999999999999.00,
+        maxValue = 9999999999999.00
+      ),
+      NonDecimalValueValidation.validate(
+        amount = requestBodyData.	totalValueGoodsSuppliedExVAT,
+        path = "/totalValueGoodsSuppliedExVAT",
+        minValue = -9999999999999.00,
+        maxValue = 9999999999999.00
+      ),
+      NonDecimalValueValidation.validate(
+        amount = requestBodyData.totalAcquisitionsExVAT,
+        path = "/totalAcquisitionsExVAT",
+        minValue = -9999999999999.00,
+        maxValue = 9999999999999.00
+      ),
+      DateFormatValidation.validate(requestBodyData.receivedAt),
+      UniqueIDValidation.validate(requestBodyData.uniqueId)
+    )
+  }
+
+  override def validate(data: SubmitReturnRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+}
---
+++ app/v1/controllers/requestParsers/validators/validations/DecimalValueValidation.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators.validations
+
+import v1.models.errors.{MtdError, ValueFormatError}
+
+object DecimalValueValidation extends ValueFormatErrorMessages {
+
+  def validateOptional(amount: Option[BigDecimal],
+                       maxScale: Int = 2,
+                       minValue: BigDecimal = 0,
+                       maxValue: BigDecimal = 9999999999999.99,
+                       path: String,
+                       message: String = BIG_DECIMAL_MINIMUM_INCLUSIVE): List[MtdError] = amount match {
+    case None => NoValidationErrors
+    case Some(value) => validate(
+      amount = value,
+      maxScale = maxScale,
+      minValue = minValue,
+      maxValue = maxValue,
+      path = path,
+      message = message
+    )
+  }
+
+  def validate(amount: BigDecimal,
+               maxScale: Int = 2,
+               minValue: BigDecimal = 0,
+               maxValue: BigDecimal = 9999999999999.99,
+               path: String,
+               message: String = BIG_DECIMAL_MINIMUM_INCLUSIVE): List[MtdError] = {
+
+    val scaleCheck = !(amount.scale > maxScale)
+    val rangeCheck = !(amount > maxValue || amount < minValue)
+
+    if (rangeCheck && scaleCheck) NoValidationErrors else List(
+      ValueFormatError.copy(
+        message = message,
+        paths = Some(Seq(path))
+      )
+    )
+  }
+}
